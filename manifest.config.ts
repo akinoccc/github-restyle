f326b9b40199f1ba3fc8b673f242a513abefa40e
Akino <akinochen@foxmail.com>
2026-05-07 00:32:51 +0800

    release: v1.0.0

--- manifest.config.ts
+++ manifest.config.ts
@@ -7,9 +7,21 @@ const manifest: ManifestV3Export = {
   version: packageJson.version,
   description: 'Give GitHub a playful new skin, with theme switching built in.',
   permissions: ['storage'],
+  icons: {
+    16: 'icons/icon-16.png',
+    32: 'icons/icon-32.png',
+    48: 'icons/icon-48.png',
+    128: 'icons/icon-128.png',
+  },
   action: {
     default_title: 'GitHub Restyle',
     default_popup: 'src/popup/index.html',
+    default_icon: {
+      16: 'icons/icon-16.png',
+      32: 'icons/icon-32.png',
+      48: 'icons/icon-48.png',
+      128: 'icons/icon-128.png',
+    },
   },
   content_scripts: [
     {
